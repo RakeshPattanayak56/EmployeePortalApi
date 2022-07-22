65181bfe3205f16f4a55dc60f4d829e337337a51
RakeshPattanayak56 <rakeshpattanayak56@gmail.com>
2022-07-22 12:38:19 +0530

    new code

--- EmployeeLoginInfo/Email/EmailNotification.cs
+++ EmployeeLoginInfo/Email/EmailNotification.cs
@@ -1,12 +1,40 @@
-﻿using Utility.Email;
+﻿using EmployeeLoginInfo.Models;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+using Utility.Email;
+using Utility.Models;
 
 namespace EmployeeLoginInfo.Email
 {
     public class EmailNotification : IEmailNotification
     {
-        public Task SendEmail(string subject, string content, bool isBodyHtml = true)
+        private EmployeeDb2022Context _context;
+        private IOptions<EmailSetting> _emailSettings;
+        private readonly IEmployeeeDetails _employeeService;
+        public EmailNotification(EmployeeDb2022Context context,IOptions<EmailSetting> emailSettings, IEmployeeeDetails employeeService)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _emailSettings = emailSettings;
+            _employeeService = employeeService;
+        }
+        public async Task SendEmail(string subject, string content, bool isBodyHtml = true)
+        {
+            List<string> Key = new List<string>();
+            MailMessage message = new MailMessage();
+            SmtpClient smtp = new SmtpClient();
+            message.To.Add(new MailAddress("sandeepkgiri20@gmail.com", "Sandeep"));
+            message.From = new MailAddress(_emailSettings.Value.Sender, _emailSettings.Value.SenderName);
+            message.Subject = subject;
+            message.IsBodyHtml = isBodyHtml; //to make message body as html  
+            message.Body = content;
+            smtp.Port = Convert.ToInt32(_emailSettings.Value.SmtpPort);
+            smtp.Host = _emailSettings.Value.SmtpHost; 
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("rakeshpattanayak56@gmail.com", "Rakesh@123");
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Send(message);
         }
     }
 }
---
+++ EmployeeLoginInfo/Functions/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using EmployeeLoginInfo.Email;
+using EmployeeLoginInfo.Email.EmailTemplate;
+using EmployeeLoginInfo.Functions;
+using EmployeeLoginInfo.Models;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Utility.Email;
+using Utility.Models;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace EmployeeLoginInfo.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var configuration = builder.Services.BuildServiceProvider().GetService<IConfiguration>();
+            string connectionString = configuration.GetConnectionString("UserDb2021");
+            builder.Services.AddDbContext<EmployeeDb2022Context>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("UserDb2021")));
+            builder.Services.AddHttpClient();
+            builder.Services.Configure<EmailTemplate>(configuration.GetSection("EmailTemplate"));
+            builder.Services.Configure<EmailSetting>(configuration.GetSection("EmailSetting"));
+            builder.Services.AddScoped<IEmailNotification, EmailNotification>();
+
+            builder.Services.AddOptions<EmailTemplate>()
+             .Configure<IConfiguration>((settings, configuration) =>
+             {
+                 configuration.GetSection("EmailTemplate").Bind(settings);
+             });
+
+            builder.Services.AddOptions<EmailSetting>()
+             .Configure<IConfiguration>((settings, configuration) =>
+             {
+                 configuration.GetSection("EmailSetting").Bind(settings);
+             });
+        }
+    }
+}
